98244b9e97b8fcbee12cf69e7a8dd4a74e2b6e17
hyh <21827014@zju.edu.cn>
2020-07-24 17:03:15 +0800

    md编辑器可上传图片

--- src/main/java/com/itomelet/blog/controller/admin/UploadController.java
+++ src/main/java/com/itomelet/blog/controller/admin/UploadController.java
@@ -10,20 +10,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
-/**
- * @author 13
- * @qq交流群 796794009
- * @email 2449207463@qq.com
- * @link http://13blog.site
- */
+
 @Controller
 @RequestMapping("/admin")
 public class UploadController {
@@ -58,5 +55,40 @@ public class UploadController {
             return null;
         }
     }
+
+    @PostMapping("/blogs/md/uploadfile")
+    public void uploadFileByEditormd(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     @RequestParam(name = "editormd-image-file", required = true)
+                                             MultipartFile file) throws IOException, URISyntaxException {
+        String fileName = file.getOriginalFilename();
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        //生成文件名称通用方法
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Random r = new Random();
+        StringBuilder tempName = new StringBuilder();
+        tempName.append(sdf.format(new Date())).append(r.nextInt(100)).append(suffixName);
+        String newFileName = tempName.toString();
+        //创建文件
+        File destFile = new File(fileUploadDic + newFileName);
+        String fileUrl = MyBlogUtils.getHost(new URI(request.getRequestURL() + "")) + "/upload/" + newFileName;
+        File fileDirectory = new File(fileUploadDic);
+        try {
+            if (!fileDirectory.exists()) {
+                if (!fileDirectory.mkdir()) {
+                    throw new IOException("文件夹创建失败,路径为：" + fileDirectory);
+                }
+            }
+            file.transferTo(destFile);
+            request.setCharacterEncoding("utf-8");
+            response.setHeader("Content-Type", "text/html");
+            response.getWriter().write("{\"success\": 1, \"message\":\"success\",\"url\":\"" + fileUrl + "\"}");
+        } catch (UnsupportedEncodingException e) {
+            response.getWriter().write("{\"success\":0}");
+        } catch (IOException e) {
+            response.getWriter().write("{\"success\":0}");
+        }
+    }
+
 
 }
